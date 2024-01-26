0d812b717c68a5178852face40dfd2e02e846788
Spaceiii <timothee01meyer@gmail.com>
2024-01-26 12:47:52 +0100

    auth

--- connexion_db.py
+++ connexion_db.py
@@ -1,17 +1,18 @@
 from flask import Flask, request, render_template, redirect, url_for, abort, flash, session, g
 
 import pymysql.cursors
-from db import *
+import os
+from dotenv import load_dotenv
 
-
+load_dotenv()
 def get_db():
     db = getattr(g, '_database', None)
     if db is None:
         db = g._database = pymysql.connect(
-            host=HOST,
-            user=USER,
-            password=PASSWD,
-            database=DATABASE,
+            host=os.environ.get("HOST"),
+            user=os.environ.get("LOGIN"),
+            password=os.environ.get("PASSWD"),
+            database=os.environ.get("DATABASE"),
             charset='utf8mb4',
             cursorclass=pymysql.cursors.DictCursor
         )
